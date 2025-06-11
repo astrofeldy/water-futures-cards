b69300aec0f4f2622bb83776d65c669e68efc3ba
astrofeldy <hannah.feldman@anu.edu.au>
2025-06-11 16:56:29 +1000

    Create Hometest.py

---
+++ Hometest.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from PIL import Image
+import random
+import os
+
+# -------------------- PAGE SETUP --------------------
+st.set_page_config(page_title="Welcome to the Water Futures Card Deck", layout="centered")
+st.title("Welcome to the Water Futures Card Deck online portal!")
+
+st.markdown("""
+## Navigate the side bar to swap between playable activities using the 'Water Futures Cards' developed by the team at The Institute for Water Futures at The Australian National University
+---
+""")
